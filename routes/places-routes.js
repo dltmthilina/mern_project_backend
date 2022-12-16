b5259de991fe4dd3fe6c1a0b3b8a83b29ebd469e
dltmthilina <dltmthilinamaduranga@gmail.com>
2022-12-16 18:37:55 +0530

    add place specific route

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,37 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next)=>{
-    console.log('Get Request in Places');
-    res.json({ message:"It works"})
+const DUMMY_PLACES = [
+    {
+        id:'p1',
+        title:'One GalleFace',
+        description: "Most popular shopping mall in srilanka",
+        location:{
+            lat:40.2547,
+            lng:-73.1245,
+        },
+        address: "Galleface, colombo, srilanka",
+        creator:"u1"
+    },
+    {
+        id:'p1',
+        title:'One GalleFace',
+        description: "Most popular shopping mall in srilanka",
+        location:{
+            lat:40.2547,
+            lng:-73.1245,
+        },
+        address: "Galleface, colombo, srilanka",
+        creator:"u1"
+    },
+]
+
+router.get('/:pid', (req, res, next)=>{
+    const placeId = req.params.pid;
+    const place = DUMMY_PLACES.find(p=>{
+        return p.id === placeId
+    });
+    res.json({place})
 });
 
 module.exports = router;
