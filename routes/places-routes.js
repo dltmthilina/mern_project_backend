eca955f1d23f2a682a415ec0b03345673f1ed374
dltmthilina <dltmthilinamaduranga@gmail.com>
2022-12-16 19:12:06 +0530

    add route to get places by user id

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -35,4 +35,15 @@ router.get('/:pid', (req, res, next)=>{
     res.json({place})
 });
 
+
+router.get('/user/:uid',(req, res, next)=>{
+    const userId = req.params.uid;
+
+    const places = DUMMY_PLACES.find (p=>{
+        return p.creator === userId;
+    })
+
+    res.json({places});
+})
+
 module.exports = router;
