1dab527cd57d01463c61cc45d074ba2ef9db3b7e
dltmthilina <dltmthilinamaduranga@gmail.com>
2022-12-17 18:44:58 +0530

    install mongoose and connect mongodb

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
 
-const placesRoutes = require('./routes/places-routes');
-const usersRoutes = require('./routes/users-routes')
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
 
-const HttpError = require('./models/http-error');
+const HttpError = require("./models/http-error");
+
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use('/api/places',placesRoutes);
-app.use('/api/users', usersRoutes)
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
 
-app.use((req, res, next)=>{
-    const error = new HttpError("Could not find this route", 404);
-    throw error;
-})
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route", 404);
+  throw error;
+});
 
-app.use((error, req, res, next)=>{
-    if(res.headerSent){
-        return next(error)
-    }
-    res.status(error.code || 500)
-    res.json({message: error.message || 'An unknown error occured!'});
-})
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occured!" });
+});
 
-app.listen(5000);
+mongoose
+  .connect(
+    "mongodb+srv://dltm:wcaUyzFQzgRX1Q3m@cluster0.5ewihvg.mongodb.net/mern?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to the database");
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
