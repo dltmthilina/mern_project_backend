e472426d745a3b16b4a63667d4231eb5cc0eb783
dltmthilina <dltmthilinamaduranga@gmail.com>
2022-12-17 08:43:42 +0530

    import controller middleware into rutes

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,3 +1,29 @@
+const HttpError = require('../models/http-error')
+
+const DUMMY_PLACES = [
+    {
+        id:'p1',
+        title:'One GalleFace',
+        description: "Most popular shopping mall in srilanka",
+        location:{
+            lat:40.2547,
+            lng:-73.1245,
+        },
+        address: "Galleface, colombo, srilanka",
+        creator:"u1"
+    },
+    {
+        id:'p1',
+        title:'One GalleFace',
+        description: "Most popular shopping mall in srilanka",
+        location:{
+            lat:40.2547,
+            lng:-73.1245,
+        },
+        address: "Galleface, colombo, srilanka",
+        creator:"u1"
+    },
+]
 
 const getPlaceById = (req, res, next)=>{
     const placeId = req.params.pid;
@@ -10,3 +36,19 @@ const getPlaceById = (req, res, next)=>{
     }
     res.json({place})
 }
+
+const getPlacesByUserId =(req, res, next)=>{
+    const userId = req.params.uid;
+    const places = DUMMY_PLACES.find (p=>{
+        return p.creator === userId;
+    })
+
+    if(!places){
+        return next(new HttpError('Could not find a place for the provided user id.', 404)); 
+    }
+
+    res.json({places});
+}
+
+exports.getPlaceById = getPlaceById;
+exports.getPlacesByUserId = getPlacesByUserId;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,60 +1,12 @@
 const express = require('express');
 
-const HttpError = require('../models/http-error')
-
 const router = express.Router();
 
-const DUMMY_PLACES = [
-    {
-        id:'p1',
-        title:'One GalleFace',
-        description: "Most popular shopping mall in srilanka",
-        location:{
-            lat:40.2547,
-            lng:-73.1245,
-        },
-        address: "Galleface, colombo, srilanka",
-        creator:"u1"
-    },
-    {
-        id:'p1',
-        title:'One GalleFace',
-        description: "Most popular shopping mall in srilanka",
-        location:{
-            lat:40.2547,
-            lng:-73.1245,
-        },
-        address: "Galleface, colombo, srilanka",
-        creator:"u1"
-    },
-]
-
-router.get('/:pid', (req, res, next)=>{
-    const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find(p=>{
-        return p.id === placeId
-    });
-
-    if(!place){
-        throw new HttpError('Could not find a place for the provided id.', 404); 
-    }
-    res.json({place})
-});
+const placeControllers = require('../controllers/places-controller');
 
 
-router.get('/user/:uid',(req, res, next)=>{
-    const userId = req.params.uid;
+router.get('/:pid', placeControllers.getPlaceById);
 
-    const places = DUMMY_PLACES.find (p=>{
-        return p.creator === userId;
-    })
-
-    if(!places){
-    
-        return next(new HttpError('Could not find a place for the provided user id.', 404)); 
-    }
-
-    res.json({places});
-})
+router.get('/user/:uid', placeControllers.getPlacesByUserId)
 
 module.exports = router;
