93b546d652d0bcb5ce5bd0dcbaa44a5a551384fb
dltmthilina <dltmthilinamaduranga@gmail.com>
2022-12-17 15:37:05 +0530

    add validation to users data using expressvalidator

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -2,6 +2,8 @@
 const { v4: uuidv4 } = require('uuid');
 const HttpError = require('../models/http-error');
 
+const {validateResult} = require('express-validator');
+
 const DUMMY_USERS = [
     {
         id:"u1",
@@ -16,6 +18,10 @@ const getUsers=(req, res, next)=>{
 };
 
 const signup=(req, res, next)=>{
+    const errors = validateResult(req);
+    if(!errors.isEmpty()){
+        throw new HttpError("Invalid inputs, please check your data", 422)
+    }
     const {name, email, password} = req.body;
     const hasUser = DUMMY_USERS.find(u=>u.email === email)
     if(hasUser){
