74dc230a200ed48c1adb142d3787253d37f620ab
dltmthilina <dltmthilinamaduranga@gmail.com>
2022-12-21 07:30:50 +0530

    get all created users

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -2,9 +2,10 @@
 const { v4: uuidv4 } = require('uuid');
 const HttpError = require('../models/http-error');
 
-const User = require('../models/user');
+
 
 const {validationResult} = require('express-validator');
+const User = require('../models/user');
 
 const DUMMY_USERS = [
     {
@@ -15,11 +16,17 @@ const DUMMY_USERS = [
     }
 ]
 
-const getUsers=(req, res, next)=>{
-    res.json({users:DUMMY_USERS})
+const getUsers= async(req, res, next)=>{
+
+    let users
+    try {
+        users = await User.find({}, '-password')
+    } catch (err) {
+        const error = new HttpError("Fetching users failed, please try again later", 500);
+        return next(error)
+    }
+    res.json({users:users.map(user=>user.toObject({getters:true}))})
 };
-
-
 
 const signup= async (req, res, next)=>{
     const errors = validationResult(req);
