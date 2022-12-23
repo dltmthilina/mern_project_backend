177aca4e56f71e5f70ac01ca7f905d0f0d6d065b
dltmthilina <dltmthilinamaduranga@gmail.com>
2022-12-23 06:31:26 +0530

    create user successfully and create place successfully

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,10 +1,11 @@
-const { v4: uuidv4 } = require('uuid');
+const mongoose = require('mongoose');
 
 const { validationResult } = require('express-validator')
 
 const HttpError = require('../models/http-error')
 const getCoordsForAddress = require('../util/location')
 const Place = require('../models/place')
+const User = require('../models/user')
 
 let DUMMY_PLACES = [
     {
@@ -90,15 +91,30 @@ const createPlace = async (req, res, next)=>{
         creator,
     })
 
+    let user;
     try {
-         await createdPlace.save()
+        user = await User.findById(creator);
     } catch (err) {
-        const error = new HttpError("Creating place failed, Please try again",500 );
+        const error = new HttpError('Creating failed, please try again later', 500);
         return next(error);
     }
 
-  
+    if(!user){
+        const error = new HttpError('Could not find user for provided id', 404);
+        return next(error);
+    }
 
+    try {
+         const sess = await mongoose.startSession();
+         sess.startTransaction();
+         await createdPlace.save({ session: sess});
+         user.places.push(createdPlace);
+         await user.save({session:sess});
+         await sess.commitTransaction();
+    } catch (err) {
+         const error = new HttpError("Creating place failed, Please try again",500 );
+        return next(error); 
+    }
     res.status(201).json({place:createdPlace});
 }
 
@@ -120,7 +136,6 @@ const updatePlace = async (req, res, next) =>{
         return next(error)
     }
 
-   
     place.title = title;
     place.description = description;
 
@@ -139,13 +154,25 @@ const deletePlace = async (req, res, next) =>{
     const placeId= req.params.pid;
     let place;
     try {
-      place = await Place.findById(placeId);
+      place = await Place.findById(placeId).populate('creator');
     } catch (err) {
         const error= new HttpError('Something went wrong', 500);
         return next(error)
     }
 
+    if(!place){
+        const error = new HttpError('Could not find place for this id', 404);
+        return next(error);
+    }
+
     try {
+        const sess = await mongoose.startSession();
+         sess.startTransaction();
+         await place.remove({ session: sess});
+         place.creator.places.pull(place)
+         await place.creator.save({session:sess});
+         await sess.commitTransaction();
+
         await place.remove();
     } catch (err) {
         const error= new HttpError('Something went wrong', 500);
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,5 +1,5 @@
 
-const { v4: uuidv4 } = require('uuid');
+
 const HttpError = require('../models/http-error');
 
 
@@ -20,7 +20,7 @@ const getUsers= async(req, res, next)=>{
     res.json({users:users.map(user=>user.toObject({getters:true}))})
 };
 
-const signup= async (req, res, next)=>{
+const signup= async(req, res, next)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
        return next(new HttpError("Invalid inputs, please check your data", 422))
@@ -33,7 +33,7 @@ const signup= async (req, res, next)=>{
     } catch (err) {
         const error = new HttpError('Signup failed, please try again later', 500)
         return next(error);
-    }
+    } 
 
     if(existingUser){
         const error =  new HttpError("Could not create user, email already exist", 422);
@@ -47,16 +47,15 @@ const signup= async (req, res, next)=>{
         password,
         places:[]
     })
-   
 
     try {
         await createdUser.save();
     } catch (err) {
-        const error = new HttpError('Signin up failed, please try again later', 500)
-        return next(error)
+         const error = new HttpError('Signin up failed, please try again later', 500)
+        return next(error) 
     }
     
-    res.status(201).json({user:createdUser})
+    res.status(201).json({user:createdUser.toObject({getters:true})})
 };
 
 const login= async(req, res,next) =>{
